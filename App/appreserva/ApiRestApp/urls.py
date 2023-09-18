e7b0fd876e5e46ccca7d123d1b3e1825aaa76fa3
josedtl <30784219+josedtl@users.noreply.github.com>
2023-09-18 11:16:50 -0500

    gg

--- App/appreserva/ApiRestApp/urls.py
+++ App/appreserva/ApiRestApp/urls.py
@@ -3,8 +3,10 @@ from rest_framework.routers import DefaultRouter
 from .views import PersonaNaturalViewSet
 
 router = DefaultRouter()
-router.register(r'personas', PersonaNaturalViewSet)
+router.register(r"personas", PersonaNaturalViewSet)
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("", include(router.urls)),
+    path("docs/", include_docs_urls(title="Tasks API")),
 ]
