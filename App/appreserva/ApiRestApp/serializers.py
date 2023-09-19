47841e5b719a0df12dabe76d139d103ecbb6e48d
josedtl <30784219+josedtl@users.noreply.github.com>
2023-09-18 21:16:20 -0500

    d

--- App/appreserva/ApiRestApp/serializers.py
+++ App/appreserva/ApiRestApp/serializers.py
@@ -1,7 +1,56 @@
 from rest_framework import serializers
-from .models import PersonaNatural
+from .models import (
+    PersonaNatural,
+    CatalogoPersonaNatural,
+    CatalogoPersonaNaturalMedioComunicacion,
+    TipoDocumentoIdentidad,
+    Genero,
+    EstadoCivil,
+    Ubigeo,
+)
+
+
+class TipoDocumentoIdentidadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TipoDocumentoIdentidad
+        fields = "__all__"
+
+
+class GeneroSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genero
+        fields = "__all__"
+
+
+class EstadoCivilSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EstadoCivil
+        fields = "__all__"
+
+
+class UbigeoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ubigeo
+        fields = "__all__"
+
 
 class PersonaNaturalSerializer(serializers.ModelSerializer):
     class Meta:
         model = PersonaNatural
-        fields = '__all__'
+        fields = "__all__"
+
+
+class CatalogoPersonaNaturalMedioComunicacionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CatalogoPersonaNaturalMedioComunicacion
+        fields = "__all__"
+
+
+class CatalogoPersonaNaturalSerializer(serializers.ModelSerializer):
+    detalles = CatalogoPersonaNaturalMedioComunicacionSerializer(
+        many=True, read_only=True
+    )
+
+    class Meta:
+        model = CatalogoPersonaNatural
+        fields = "__all__"
--- App/appreserva/ApiRestApp/urls.py
+++ App/appreserva/ApiRestApp/urls.py
@@ -1,9 +1,27 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PersonaNaturalViewSet
+from .views import (
+    PersonaNaturalViewSet,
+    CatalogoPersonaNaturalViewSet,
+    CatalogoPersonaNaturalMedioComunicacionViewSet,
+    EstadoCivilViewSet,
+    GeneroViewSet,
+    UbigeoViewSet,
+    TipoDocumentoIdentidadViewSet,
+)
 
 router = DefaultRouter()
 router.register(r"personas", PersonaNaturalViewSet)
+router.register(r"catalogo_personanatural", CatalogoPersonaNaturalViewSet)
+router.register(
+    r"catalogo_personanaturalmediocomunicacion",
+    CatalogoPersonaNaturalMedioComunicacionViewSet,
+)
+router.register(r"EstadoCivil", EstadoCivilViewSet)
+router.register(r"Genero", GeneroViewSet)
+router.register(r"Ubigeo", UbigeoViewSet)
+router.register(r"TipoDocumentoIdentidad", TipoDocumentoIdentidadViewSet)
+
 from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
